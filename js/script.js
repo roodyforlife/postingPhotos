dfceec19c575a81542c341b7cfcaa8540bb3b814
roodyforlife <79136277+roodyforlife@users.noreply.github.com>
2022-04-16 20:57:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function burger()
+{
+    document.getElementById('header__burger').classList.toggle('active')
+    document.getElementById('blind').classList.toggle('active__blind')
+}
+
+function blind_clicked(){
+    document.getElementById('header__burger').classList.toggle('active')
+    document.getElementById('blind').classList.toggle('active__blind')
+}
